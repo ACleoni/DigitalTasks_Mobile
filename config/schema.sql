d41bfadb8134a21520210b9fe54f74cef304d3f5
Tyler Campbell <tcampb30@gmail.com>
2018-08-29 23:50:17 -0400

    updated schema.sql

--- config/schema.sql
+++ config/schema.sql
@@ -1,7 +1,11 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(60) NOT NULL,
-    password VARCHAR(60) NOT NULL,
-    createdAt TIMESTAMP NOT NULL,
-    updatedAt TIMESTAMP NOT NULL
-);
+CREATE TABLE IF NOT EXISTS "users" (
+    "id" SERIAL , 
+    "email" VARCHAR(255) NOT NULL, 
+    "password" VARCHAR(255) NOT NULL, 
+    "emailConfirmed" BOOLEAN NOT NULL DEFAULT false, 
+    "confirmationEmailToken" VARCHAR(255) NOT NULL, 
+    "confirmationEmailExpirationDate" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT '2018-08-30 07:45:28.025 +00:00', 
+    "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
+    "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL, 
+    UNIQUE ("email"), 
+    PRIMARY KEY ("id"));
