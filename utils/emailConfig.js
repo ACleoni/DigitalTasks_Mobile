23701141b264f35a393ad71966f11f156fe1d081
Tyler Campbell <tcampb30@gmail.com>
2018-09-07 12:54:06 -0400

    Added emailConfig util

---
+++ utils/emailConfig.js
@@ -0,0 +1,17 @@
+const apiToken = process.env.SENDGRID_TOKEN || require('../config/secretKey').emailKey;
+const mailtrapAuth = process.env.MAILTRAP_AUTH || require('../config/secretKey').mailtrap.auth;
+const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail');
+
+exports.mailTrapMailer = (() => {
+    return transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: mailtrapAuth
+      });
+})();
+
+exports.sgMail = (() => {
+    sgMail.setApiKey(apiToken);
+    return sgMail;
+})();
